7afce73cec17ade1a0bd5e3d2363e5439d2622aa
Michael Pumo <hello@michaelpumo.com>
2024-11-12 18:13:42 +0000

    Update colour tokens.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#EAFF01",
+        green: "#ADF454",
+        blue: "#51DBEA",
+        purple: "#A58EF7",
+        navy: "#1C1D2C",
+        cream: "#F7F7F0",
+        grey: "#BBBBBB",
+      },
+    },
+  },
+  plugins: [],
+};
