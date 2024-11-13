67d77f4e6c750aba5b2d7b51702f36f048e8c2f9
Michael Pumo <hello@michaelpumo.com>
2024-11-13 17:24:11 +0000

    Generate favicons.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import favicons from "astro-favicons";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    favicons({
+      masterPicture: "./src/icon/favicon.svg",
+      emitAssets: true,
+      faviconsDarkMode: false,
+      appName: "Yellowball",
+      appShortName: "Yellowball",
+      appDescription: "",
+      lang: "en-GB",
+      background: "transparent",
+      theme_color: "#fff",
+    }),
     tailwind({
       /**
        * Removes automatic insertion of TW styles, base/component/utilities.
