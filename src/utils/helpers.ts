7e6def8d7802a686dd98c1ba2ab928e21830599c
Michael Pumo <hello@michaelpumo.com>
2024-11-30 17:41:16 +0000

    Debounce util.

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,16 +1,17 @@
 type Procedure = (...args: any[]) => void;
 
 function debounce<T extends Procedure>(func: T, wait: number): (...args: Parameters<T>) => void {
-    let timeoutId: ReturnType<typeof setTimeout> | undefined;
+  let timeoutId: ReturnType<typeof setTimeout> | undefined;
 
-    return function (...args: Parameters<T>) {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func(...args);
-        }, wait);
-    };
+  return function (...args: Parameters<T>) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, wait);
+  };
 }
 
 
