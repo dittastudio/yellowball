5a3076525e5e0b19d275a14d796c533a22fb0227
Will The Dev <hello@willthe.dev>
2024-12-10 20:00:09 +0000

    toggle sub amends

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -16,3 +16,21 @@ interface TagObject {
 
 type ColorTypes = 'current' | 'transparent' | 'offblack' | 'black' | 'white' | 'yellow' | 'green' | 'blue' | 'blue-google' | 'purple' | 'navy' | 'navy-light' | 'navy-lighter' | 'navy-lightest' | 'cream' | 'grey' | 'grey-light'
 type ShapeTypes = 'circle' | 'square' | 'triangle'
+
+interface HeaderNavigationSubMenuItem {
+  text: string;
+  url: string;
+}
+
+interface HeaderNavigationSubMenu {
+  heading: string;
+  items: HeaderNavigationSubMenuItem[];
+}
+
+interface HeaderNavigationItem {
+  text: string;
+  url?: string;
+  type?: 'default' | 'alt';
+  color?: 'transparent' | 'navy' | 'yellow';
+  subMenu?: HeaderNavigationSubMenu[];
+}
