a5f68339c391e9967160cbe9c40f3a067ef3bf22
Will The Dev <hello@willthe.dev>
2025-01-21 11:33:48 +0000

    removed expose colors

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,6 +6,7 @@ interface Window {
 type ColorTypes = 'current' | 'transparent' | 'offblack' | 'black' | 'white' | 'yellow' | 'green' | 'blue' | 'blue-google' | 'purple' | 'navy' | 'navy-light' | 'navy-lighter' | 'navy-lightest' | 'cream' | 'grey' | 'grey-light'
 type ShapeTypes = 'circle' | 'square' | 'triangle' | 'pentagon'
 type ThemeTypes = 'light' | 'dark'
+type ColorMainTypes = 'yellow' | 'green' | 'blue' | 'purple'
 
 interface ImageObject {
   src: string
