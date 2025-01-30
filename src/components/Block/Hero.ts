8192313c9d8af5f86f1533c9de38161b885ebbe6
Will The Dev <hello@willthe.dev>
2025-01-31 00:06:58 +0100

    optional logo ani

--- src/components/Block/Hero.ts
+++ src/components/Block/Hero.ts
@@ -46,31 +46,37 @@ function FnHero() {
       });
     }
 
-    if (hero) {
+    const heroLogoContainer = hero.querySelector('[data-js-hero-logo]') as HTMLElement;
+
+    if (hero && heroLogoContainer) {
+      const heroLogo = heroLogoContainer.querySelector('[data-js="ui-logo"]') as HTMLElement;
+
       const header = document.querySelector('[data-js="header"]') as HTMLElement;
-      const heroLogo = hero.querySelector('[data-js-hero-logo]') as HTMLElement;
-      const heroLogoSVG = heroLogo.querySelector('[data-js="ui-logo"]') as HTMLElement;
+      const layoutLines = document.querySelector('[data-js="layout-lines"]') as HTMLElement;
       const heroContent = hero.querySelector('[data-js-hero-content]') as HTMLElement;
       const cursors = hero.querySelector('[data-js="cursors"]') as HTMLElement;
-      const layoutLines = document.querySelector('[data-js="layout-lines"]') as HTMLElement;
 
       const tl = gsap.timeline();
+
+      gsap.set(heroLogo, {
+        opacity: 0,
+      });
+
+      gsap.set(heroLogoContainer, {
+        y: window.innerHeight / 2 - ((heroLogoContainer.offsetHeight * 1.5) + header.offsetHeight),
+        scale: 1.85,
+      });
 
       gsap.set(header, {
         y: '-100%',
       });
 
-      gsap.set(heroLogoSVG, {
-        opacity: 0,
+      gsap.set(layoutLines, {
+        '--layout-line-top-opacity': 0,
       });
 
       gsap.set(hero, {
         height: 'calc(100vh - var(--header-height))',
-      });
-
-      gsap.set(heroLogo, {
-        y: window.innerHeight / 2 - (heroLogo.offsetHeight + header.offsetHeight),
-        scale: 1.85,
       });
 
       gsap.set(heroContent, {
@@ -81,64 +87,56 @@ function FnHero() {
         scale: 2,
       });
 
-      gsap.set(layoutLines, {
-        '--layout-line-top-opacity': 0,
-      });
-
       const duration = 1.5;
-      const easing = 'elastic.inOut(1, 1.5)';
+      const ease = 'elastic.inOut(1, 1.5)';
       const delay = 1;
 
-      tl.to(heroLogoSVG, {
-        opacity: 1,
-        duration,
-        ease: easing,
-        delay: 0.5,
-        onComplete: () => {
-          heroLogoSVG.classList.add('has-animation');
-        },
-      }).to(header, {
-        opacity: 1,
-        y: 0,
-        duration,
-        ease: easing,
-        delay,
-      }).to(hero, {
-        height: 'auto',
-        duration,
-        ease: easing,
-      },
-        '<',
-      ).to(heroLogo, {
-        y: 0,
-        scale: 1,
-        duration,
-        ease: easing,
-      },
-        '<',
-      ).to(heroContent, {
-        opacity: 1,
-        y: 0,
-        duration,
-        ease: easing,
-      },
-        '<',
-      ).to(layoutLines, {
-        '--layout-line-top-opacity': 1,
-        duration,
-        ease: easing,
-      },
-        '<',
-      ).to(cursors, {
-        scale: 1,
-        duration,
-        ease: easing,
-        onComplete: () => {
-          gsap.set(header, { clearProps: "all" });
-        },
-      },
-        '<',
-      );
+      tl
+        .to(heroLogo, {
+          opacity: 1,
+          duration: 1,
+          ease: "power3.out",
+          delay: 0.5,
+          onComplete: () => {
+            heroLogo.classList.add('has-animation');
+          },
+        })
+        .to(heroLogoContainer, {
+          y: 0,
+          scale: 1,
+          duration,
+          ease,
+          delay,
+        })
+        .to(header, {
+          y: 0,
+          duration,
+          ease,
+        }, '<')
+        .to(hero, {
+          height: 'auto',
+          duration,
+          ease,
+        }, '<')
+        .to(heroContent, {
+          opacity: 1,
+          y: 0,
+          duration,
+          ease,
+        }, '<')
+        .to(layoutLines, {
+          '--layout-line-top-opacity': 1,
+          duration,
+          ease,
+        }, '<')
+        .to(cursors, {
+          scale: 1,
+          duration,
+          ease,
+          onComplete: () => {
+            gsap.set(header, { clearProps: "all" });
+          },
+        }, '<')
     }
 
     // Alpine.data('hero', () => ({
