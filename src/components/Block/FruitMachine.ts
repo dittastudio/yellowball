ca40ce6bf49a6374a8fe84ccc2dfe8d984b19955
Michael Pumo <hello@michaelpumo.com>
2025-02-02 18:56:56 +0000

    Housekeeping.

--- src/components/Block/FruitMachine.ts
+++ src/components/Block/FruitMachine.ts
@@ -1,5 +1,4 @@
 import confetti from 'canvas-confetti';
-
 import { sleep } from '@/utils/helpers';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
@@ -19,6 +18,22 @@ function FnFruitMachine() {
     canvas.style.minHeight = `${minHeight}px`;
   }
 
+  const triangle = confetti.shapeFromPath({
+    path: 'M1.50815 70.7666C-0.8649 74.8682 2.09484 80 6.83342 80H85.1666C89.9052 80 92.8649 74.8682 90.4918 70.7666L51.3253 3.0713C50.1406 1.02377 48.0703 0 46 0C43.9297 0 41.8594 1.02377 40.6747 3.0713L1.50815 70.7666ZM46 18.4376L74.4991 67.6953H17.5009L46 18.4376Z',
+  });
+
+  const square = confetti.shapeFromPath({
+    path: 'M5.52002 5.32373H27.52V27.3237H5.52002V5.32373ZM3.02002 0.32373C1.63931 0.32373 0.52002 1.44302 0.52002 2.82373V29.8237C0.52002 31.2044 1.63931 32.3237 3.02002 32.3237H30.02C31.4007 32.3237 32.52 31.2044 32.52 29.8237V2.82373C32.52 1.44302 31.4007 0.32373 30.02 0.32373H3.02002Z',
+  });
+
+  const circle = confetti.shapeFromPath({
+    path: 'M16.5 27C10.4249 27 5.5 22.0751 5.5 16C5.5 9.92487 10.4249 5 16.5 5C22.5751 5 27.5 9.92487 27.5 16C27.5 22.0751 22.5751 27 16.5 27ZM16.5 32C25.3366 32 32.5 24.8366 32.5 16C32.5 7.16344 25.3366 0 16.5 0C7.66344 0 0.5 7.16344 0.5 16C0.5 24.8366 7.66344 32 16.5 32Z',
+  });
+
+  const pentagon = confetti.shapeFromPath({
+    path: 'M17.0001 5.58654L5.69281 13.7739L10.0049 27H23.9953L28.3074 13.7739L17.0001 5.58654ZM18.4663 0.475082L32.715 10.7922C33.5942 11.4288 33.9622 12.56 33.6257 13.5921L28.1866 30.2749C27.8512 31.3038 26.8919 32 25.8098 32H8.19046C7.10832 32 6.14903 31.3038 5.8136 30.2749L0.374527 13.5921C0.0380617 12.56 0.406002 11.4288 1.2852 10.7922L15.5339 0.475082C16.4087 -0.158361 17.5915 -0.158361 18.4663 0.475082Z',
+  });
+
   machines.forEach(machine => {
     const canvas = machine.querySelector('canvas') as HTMLCanvasElement;
 
@@ -32,26 +47,13 @@ function FnFruitMachine() {
         useWorker: true
       });
 
-      const triangle = confetti.shapeFromPath({
-        path: 'M1.50815 70.7666C-0.8649 74.8682 2.09484 80 6.83342 80H85.1666C89.9052 80 92.8649 74.8682 90.4918 70.7666L51.3253 3.0713C50.1406 1.02377 48.0703 0 46 0C43.9297 0 41.8594 1.02377 40.6747 3.0713L1.50815 70.7666ZM46 18.4376L74.4991 67.6953H17.5009L46 18.4376Z',
-      });
-      const square = confetti.shapeFromPath({
-        path: 'M5.52002 5.32373H27.52V27.3237H5.52002V5.32373ZM3.02002 0.32373C1.63931 0.32373 0.52002 1.44302 0.52002 2.82373V29.8237C0.52002 31.2044 1.63931 32.3237 3.02002 32.3237H30.02C31.4007 32.3237 32.52 31.2044 32.52 29.8237V2.82373C32.52 1.44302 31.4007 0.32373 30.02 0.32373H3.02002Z',
-      });
-      const circle = confetti.shapeFromPath({
-        path: 'M16.5 27C10.4249 27 5.5 22.0751 5.5 16C5.5 9.92487 10.4249 5 16.5 5C22.5751 5 27.5 9.92487 27.5 16C27.5 22.0751 22.5751 27 16.5 27ZM16.5 32C25.3366 32 32.5 24.8366 32.5 16C32.5 7.16344 25.3366 0 16.5 0C7.66344 0 0.5 7.16344 0.5 16C0.5 24.8366 7.66344 32 16.5 32Z',
-      });
-      const pentagon = confetti.shapeFromPath({
-        path: 'M17.0001 5.58654L5.69281 13.7739L10.0049 27H23.9953L28.3074 13.7739L17.0001 5.58654ZM18.4663 0.475082L32.715 10.7922C33.5942 11.4288 33.9622 12.56 33.6257 13.5921L28.1866 30.2749C27.8512 31.3038 26.8919 32 25.8098 32H8.19046C7.10832 32 6.14903 31.3038 5.8136 30.2749L0.374527 13.5921C0.0380617 12.56 0.406002 11.4288 1.2852 10.7922L15.5339 0.475082C16.4087 -0.158361 17.5915 -0.158361 18.4663 0.475082Z',
-      });
-
       const realisticBurst = (particleRatio: number, direction: 'left' | 'right', opts: any) => {
         myConfetti({
           ...opts,
           origin: { x: direction === 'left' ? -0.1 : 1.1, y: 0.6 },
           angle: direction === 'left' ? 30 : 150,
           particleCount: Math.floor(200 * particleRatio),
-          scalar: 1,
+          scalar: 1.5,
         })
       }
 
@@ -83,8 +85,8 @@ function FnFruitMachine() {
       }
 
       const doSpin = async () => {
-        const stripItems = machine.querySelectorAll('[data-js-fruit-machine-strip-item]');
-        const stripImages = machine.querySelectorAll('[data-js-fruit-machine-strip-image]');
+        const stripItems = machine.querySelectorAll('li');
+        const stripImages = machine.querySelectorAll('li img');
 
         const tl = gsap.timeline()
 
@@ -107,10 +109,12 @@ function FnFruitMachine() {
           }, '<')
 
         await sleep(3300);
+
         burst('left')
         burst('right')
 
         await sleep(750);
+
         machine.classList.add('spin-complete');
 
         setTimeout(() => {
