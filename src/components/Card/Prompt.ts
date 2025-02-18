308aad78b46ec23d86e7e326b914e51a1d427b15
Michael Pumo <hello@michaelpumo.com>
2025-02-18 22:38:06 +0000

    Disable scrolltrigger on small devices.

--- src/components/Card/Prompt.ts
+++ src/components/Card/Prompt.ts
@@ -5,24 +5,36 @@ function FnPrompt() {
     '[data-js="card-prompt"]',
   ) as NodeListOf<HTMLElement>;
 
-  cards.forEach(card => {
-    const shape = card.querySelector('[data-js-shape]') as HTMLElement;
+  const mm = gsap.matchMedia()
 
-    if (shape) {
-      const { height: cardHeight } = card.getBoundingClientRect();
-      const { height: shapeHeight } = shape.getBoundingClientRect();
+  mm.add({
+    isSm: "screen and (min-width: 640px)"
+  }, (context) => {
+    const { conditions } = context;
 
-      gsap.to(shape, {
-        y: `-${cardHeight - shapeHeight / 2}`,
-        scrollTrigger: {
-          markers: false,
-          trigger: card,
-          scrub: 1,
-          start: 'top bottom',
-          end: 'bottom top',
-        },
-      });
+    if (!conditions?.isSm) {
+      return
     }
+
+    cards.forEach(card => {
+      const shape = card.querySelector('[data-js-shape]') as HTMLElement;
+
+      if (shape) {
+        const { height: cardHeight } = card.getBoundingClientRect();
+        const { height: shapeHeight } = shape.getBoundingClientRect();
+
+        gsap.to(shape, {
+          y: `-${cardHeight - shapeHeight / 2}`,
+          scrollTrigger: {
+            markers: false,
+            trigger: card,
+            scrub: 1,
+            start: 'top bottom',
+            end: 'bottom top',
+          },
+        });
+      }
+    });
   });
 }
 
